85fb7da5710fb3ebf085584ec0b5e49794af0b14
David Calhoun <github@davidcalhoun.me>
2023-07-04 22:18:45 -0400

    feat: Remove archive file after export

--- bin/export.js
+++ bin/export.js
@@ -18,6 +18,15 @@ const zip = new AdmZip(zipFilePath);
 try {
   // Extract all files from the archive
   zip.extractAllTo(destinationPath, true);
+
+  // Remove the archive file
+  fs.unlink(zipFilePath, (error) => {
+    if (error) {
+      console.error("Error removing the ZIP file:", error);
+    } else {
+      console.log("Archive file removed successfully.");
+    }
+  });
   console.log("Archive extracted successfully.");
 } catch (error) {
   console.error("Error extracting the archive:", error);
