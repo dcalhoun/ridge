282f3efb93e50020fb652882152726fc4f87ece2
David Calhoun <github@davidcalhoun.me>
2023-07-21 08:21:06 -0400

    feat: Add Horizontal Post Navigation pattern

---
+++ patterns/horizontal-post-navigation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Horizontal Post Navigation
+ * Slug: ridge/horizontal-post-navigation
+ * Categories: ridge
+ */
+?>
+<!-- wp:separator {"align":"full","className":"is-style-wide"} -->
+<hr class="wp-block-separator alignfull has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"stretch"}} -->
+<div class="wp-block-group alignfull"><!-- wp:post-navigation-link {"type":"previous","label":"Previous","showTitle":true} /-->
+
+<!-- wp:post-navigation-link {"label":"Next","showTitle":true} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"align":"full","className":"is-style-wide"} -->
+<hr class="wp-block-separator alignfull has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
